0ae0d99196d91167ef5f556d4392715461f46280
guilhermesvm <guilhermesvmachado@gmail.com>
2025-05-10 16:03:32 -0300

    fix: correct tests in main page

--- AdoPet/cypress/e2e/main-page/main-page.cy.js
+++ AdoPet/cypress/e2e/main-page/main-page.cy.js
@@ -3,6 +3,10 @@ describe('Main page', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
   });
 
+  it('should check page title', () => {
+    cy.title().should('eq', 'AdoPet');
+  });
+
   it('should show available pets', () => {
     cy.get('.button').click();
   });
@@ -14,9 +18,21 @@ describe('Main page', () => {
 
   it('should go to login page', () => {
     cy.get('[data-test="login-button"]').click();
+    cy.url().should('include', '/login');
   });
 
   it('should go to register page', () => {
-    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="register-button"]').click();
+    cy.url().should('include', '/cadastro');
   });
+  
+  it('should middle paragraph', () => {
+    cy.contains('p', 'Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should('be.visible');
+  });
+
+  it('should check footer paragraph', () => {
+    cy.contains('p', 'Quem ama adota!').should('be.visible');
+  });
+
+  
 });
