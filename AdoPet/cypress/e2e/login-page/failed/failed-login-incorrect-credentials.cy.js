59773449291a98eeeb74d118af253dbb72d82045
guilhermesvm <guilhermesvmachado@gmail.com>
2025-05-23 22:31:42 -0300

    feat: add Stub on login failed flow

--- AdoPet/cypress/e2e/login-page/failed/failed-login-incorrect-credentials.cy.js
+++ AdoPet/cypress/e2e/login-page/failed/failed-login-incorrect-credentials.cy.js
@@ -6,20 +6,28 @@ describe('Login Page', () => {
 
     beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/login');
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+        statusCode: 400}).as('stubPost');
     });
 
     it('Failed Login - incorrect credentials', () => {
-        cy.login(INCORRECT_EMAIL, INCORRECT_PASSWORD)
+        cy.login(INCORRECT_EMAIL, INCORRECT_PASSWORD);
+        cy.wait('@stubPost');
         cy.get('p.error').should('be.visible').and('contain', 'Falha no login. Consulte suas credenciais.');
+        
     });
 
     it('Failed Login - incorrect email and correct password', () => {
-        cy.login(INCORRECT_EMAIL, VALID_PASSWORD)
+        cy.login(INCORRECT_EMAIL, VALID_PASSWORD);
+        cy.wait('@stubPost');
         cy.get('p.error').should('be.visible').and('contain', 'Falha no login. Consulte suas credenciais.');
     });
 
     it('Failed Login - correct email and incorrect password', () => {
-        cy.login(VALID_EMAIL, INCORRECT_PASSWORD)
+
+        
+        cy.login(VALID_EMAIL, INCORRECT_PASSWORD);
+        cy.wait('@stubPost');
         cy.get('p.error').should('be.visible').and('contain', 'Falha no login. Consulte suas credenciais.');
     });
 });
