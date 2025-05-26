fc54dce49919e365f8a599e4fe47d6949b50e3c1
guilhermesvm <guilhermesvmachado@gmail.com>
2025-05-26 00:00:39 -0300

    Final commit

--- AdoPet/cypress/e2e/api/api-messages.cy.js
+++ AdoPet/cypress/e2e/api/api-messages.cy.js
@@ -3,16 +3,12 @@ describe('API AdoPet', () => {
         cy.request({
             method: 'GET',
             url: "https://adopet-api-i8qu.onrender.com/mensagem/09fc8914-d9d9-4ee6-a460-4a4221559cc6",
-            headers: { 
-                Authorization: `Bearer ${Cypress.env('API_TOKEN')}`
-            },
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.eq(200);
             expect(res.body).is.not.empty;
             expect(res.body).to.have.property('msg');
             expect(res.body).to.be.an('object');
-            //expect(res.duration).to.be.lte(expectedTime)
         })
     })
-
 })
